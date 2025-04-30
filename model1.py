03a1c49dfd0bba790c3cad5221b30a784d337453
marcon21 <marcon859@gmail.com>
2025-04-30 17:42:16 +0200

    inital pipeline

---
+++ model1.py
@@ -0,0 +1,40 @@
+from model_template import ModelTemplate
+from torch.nn import functional as F
+from torch import nn
+
+
+class Model1(ModelTemplate):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        file_path: str = "./model_saves/model1.pth",
+        device=None,
+    ):
+        """
+        Initializes the model with the specified input and output dimensions, file path for saving the model,
+        and the device to be used for computation.
+        Args:
+            input_dim (int): The dimensionality of the input features.
+            output_dim (int): The dimensionality of the output features.
+            file_path (str, optional): The file path where the model will be saved. Defaults to "./model_saves/model1.pth".
+            device (torch.device, optional): The device to be used for computation (e.g., 'cpu' or 'cuda'). Defaults to None.
+        """
+        super().__init__(file_path, device)
+
+        self.network = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, output_dim),
+        )
+
+    def forward(self, x):
+        x = self.network(x)
+        return x
+
+
+if __name__ == "__main__":
+    model = Model1(input_dim=10, output_dim=1)
+    print(model)

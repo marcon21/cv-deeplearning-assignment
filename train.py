a3598319a4693f4080021fccea84f4370d4e240b
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2025-05-13 11:50:17 +0200

    train lr

--- train.py
+++ train.py
@@ -149,10 +149,21 @@ if __name__ == "__main__":
 
         print(f"Training {model.model_name}...")
         if model.model_name == "Swin":
-            optimizer = optim.AdamW([
-                    {"params": model.backbone.parameters(), "lr": 5e-5},
-                    {"params": model.decoder.parameters(), "lr": lr},
-                ], weight_decay=0.01)
+            if args.backbone == "tiny":
+                optimizer = optim.AdamW([
+                        {"params": model.backbone.parameters(), "lr": 5e-5},
+                        {"params": model.decoder.parameters(), "lr": lr},
+                    ], weight_decay=0.01)
+            elif args.backbone == "base":
+                optimizer = optim.AdamW([
+                        {"params": model.backbone.parameters(), "lr": 1e-5},
+                        {"params": model.decoder.parameters(), "lr": lr},
+                    ], weight_decay=0.01)
+            elif args.backbone == "small":
+                optimizer = optim.AdamW([
+                        {"params": model.backbone.parameters(), "lr": 1e-5},
+                        {"params": model.decoder.parameters(), "lr": lr},
+                    ], weight_decay=0.01)
             scheduler = get_cosine_schedule_with_warmup(
                 optimizer,
                 num_warmup_steps=int(0.1 * len(train_loader) * run_params["epochs"]),
